477b015f8b88952e00570cd8a3e54ca7d2ee832f
wei chen <weichen9083@gmail.com>
2021-06-21 01:05:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210621010124) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
